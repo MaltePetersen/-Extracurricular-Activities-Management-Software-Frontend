30780d103ab244c4240850827842b71d9ed12310
hendrik.garner <garner@posteo.at>
2019-12-09 12:30:39 +0100

    Registrierungsseite mit Eingabevalidierung abgeschlossen. Werte werden bisher nur in Console ausgegeben, nicht ans Backend übermittelt

--- src/app/pages/erziehungsberechtigte/registrierung/registrierung.page.ts
+++ src/app/pages/erziehungsberechtigte/registrierung/registrierung.page.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgForm, Validators, FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { AuthenticationService } from 'src/app/services/authentication.service';
-import { AlertService} from 'src/app/services/alert.service';
-import { UsernameValidator} from 'src/app/pages/erziehungsberechtigte/registrierung/username.validator';
+import { AlertService } from 'src/app/services/alert.service';
+import { UsernameValidator } from 'src/app/pages/erziehungsberechtigte/registrierung/username.validator';
 import { PasswordValidator } from 'src/app/pages/erziehungsberechtigte/registrierung/password.validator';
 import { PhoneValidator } from 'src/app/pages/erziehungsberechtigte/registrierung/phone.validator';
-import {CountryPhone} from 'src/app/models/country-phone.model';
+import { CountryPhone } from 'src/app/models/country-phone.model';
 
 
 
@@ -22,7 +22,7 @@ export class RegistrierungPage implements OnInit {
   country_phone_group: FormGroup;
   countries: Array<CountryPhone>;
   responseData: any;
-  userData = {'username': '','password': '', 'name': '','email': ''};
+  userData = { 'username': '', 'password': '', 'name': '', 'email': '' };
 
   constructor(public formBuilder: FormBuilder, private router: Router, private auth: AuthenticationService, private alertService: AlertService) { }
 
@@ -58,7 +58,7 @@ export class RegistrierungPage implements OnInit {
         UsernameValidator.validUsername,
         Validators.maxLength(25),
         Validators.minLength(5),
-        Validators.pattern('^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$'),
+        Validators.pattern('^(?=.*[a-zA-Z])||(?=.*[0-9])[a-zA-Z0-9]+$'),
         Validators.required
       ])),
       matching_passwords: this.matching_passwords_group,
@@ -69,13 +69,16 @@ export class RegistrierungPage implements OnInit {
         Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$')
       ])),
       country_phone: this.country_phone_group,
-      rolle: new FormControl ('', Validators.required),
+      rolle: new FormControl('', Validators.required),
       nameChild: new FormControl('', Validators.required),
       lastnameChild: new FormControl('', Validators.required),
       birthday: new FormControl('', Validators.compose([
         Validators.required,
-        Validators.pattern('^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$')
+        Validators.pattern('^[0-9]{2}[\.]{1}[0-9]{2}[\.]{1}[0-9]{4}$')
+        //Validators.pattern('^(((0[1-9]|[12]\d|3[01])\/(0[13578]|1[02])\/((19|[2-9]\d)\d{2}))|((0[1-9]|[12]\d|30)\/(0[13456789]|1[012])\/((19|[2-9]\d)\d{2}))|((0[1-9]|1\d|2[0-8])\/02\/((19|[2-9]\d)\d{2}))|(29\/02\/((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))))$')
       ])),
+      school: new FormControl('', Validators.required),
+      schoolClass: new FormControl('', Validators.required)
     });
   }
 
@@ -83,53 +86,59 @@ export class RegistrierungPage implements OnInit {
 
   validation_messages = {
     'username': [
-      { type: 'required', message: 'Username is required.' },
-      { type: 'minlength', message: 'Username must be at least 5 characters long.' },
-      { type: 'maxlength', message: 'Username cannot be more than 25 characters long.' },
-      { type: 'pattern', message: 'Your username must contain only numbers and letters.' },
-      { type: 'validUsername', message: 'Your username has already been taken.' }
+      { type: 'required', message: 'Nutzername muss angegeben werden.' },
+      { type: 'minlength', message: 'Nutzername muss mind. 5 Zeichen lang sein.' },
+      { type: 'maxlength', message: 'Nutzername darf nicht länger als 25 Zeichen sein.' },
+      { type: 'pattern', message: 'Nutzername darf nur aus Zahlen oder Buchstaben bestehen.' },
+      { type: 'validUsername', message: 'Nutzername ist bereits vergeben.' }
     ],
     'password': [
-      { type: 'required', message: 'Password is required.' },
-      { type: 'minlength', message: 'Password must be at least 5 characters long.' },
-      { type: 'pattern', message: 'Your password must contain at least one uppercase, one lowercase, and one number.' }
+      { type: 'required', message: 'Passwort wird benötigt.' },
+      { type: 'minlength', message: 'Passwort muss mind. 5 Zeichen lang sein.' },
+      { type: 'pattern', message: 'Passwort muss mind. einen Großbuchstaben, einen Kleinbuchstaben und eine Nummer beinhalten.' }
     ],
     'confirm_password': [
-      { type: 'required', message: 'Confirm password is required.' }
+      { type: 'required', message: 'Passwortbestätigung ist notwendig' }
     ],
     'matching_passwords': [
-      { type: 'areEqual', message: 'Password mismatch.' }
+      { type: 'areEqual', message: 'Passwort stimmt nicht überein.' }
     ],
     'name': [
-      { type: 'required', message: 'Name is required.' }
+      { type: 'required', message: 'Vorname ist notwendig.' }
     ],
     'lastname': [
-      { type: 'required', message: 'Last name is required.' }
+      { type: 'required', message: 'Nachname ist notwendig.' }
     ],
     'email': [
-      { type: 'required', message: 'Email is required.' },
-      { type: 'pattern', message: 'Please wnter a valid email.' }
+      { type: 'required', message: 'Email ist notwendig.' },
+      { type: 'pattern', message: 'Gib eine gültige Email ein.' }
     ],
     'phone': [
-      { type: 'required', message: 'Phone is required.' },
-      { type: 'validCountryPhone', message: 'The phone is incorrect for the selected country.' }
+      { type: 'required', message: 'Telefonnummer ist notwendig.' },
+      { type: 'validCountryPhone', message: 'Keine gültige Telefonnummer.' }
     ],
     'rolle': [
-      { type: 'required', message: 'Rolle is required.' },
+      { type: 'required', message: 'Rolle muss ausgewählt werden.' },
     ],
     'nameChild': [
-      { type: 'required', message: 'Name is required.' }
+      { type: 'required', message: 'Name ist notwendig.' }
     ],
     'lastnameChild': [
-      { type: 'required', message: 'Last name is required.' }
+      { type: 'required', message: 'Nachname ist notwendig.' }
     ],
     'birthday': [
-      { type: 'required', message: 'Geburtstag is required.' },
-      { type: 'pattern', message: 'Das Datum muss im Format TT.MM.JJJJ angegeben werden.' }
+      { type: 'required', message: 'Geburtstag ist notwendig.' },
+      { type: 'pattern', message: 'Datum muss im Format TT.MM.JJJJ angegeben werden.' }
+    ],
+    'school': [
+      { type: 'required', message: 'Schule ist notwendig.' },
+    ],
+    'schoolClass': [
+      { type: 'required', message: 'Klasse ist notwendig.' },
     ],
   };
 
-  onSubmit(values){
+  onSubmit(values) {
     console.log(values);
   }
 
