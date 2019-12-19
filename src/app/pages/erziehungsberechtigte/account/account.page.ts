bff85e82255b5974a3655a146990415bd8e9e6e8
hendrik.garner <garner@posteo.at>
2019-12-19 15:50:00 +0100

    Account Seite erstellt

--- src/app/pages/erziehungsberechtigte/account/account.page.ts
+++ src/app/pages/erziehungsberechtigte/account/account.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { AlertService } from 'src/app/services/alert.service';
 
 @Component({
   selector: 'app-account',
@@ -7,9 +10,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountPage implements OnInit {
 
-  constructor() { }
+  constructor(private alertController: AlertController,  private router: Router,private alertService: AlertService) { }
+
+  username: string;
+  fname: string;
 
   ngOnInit() {
+    this.username ="Peter";
+    this.fname ="Klaus-Dieter";
+  }
+
+  async saveChanges(){
+    console.log(this.schoolClass)
+    console.log(this.username)
+    console.log(this.fname)
+    const alert = await this.alertController.create({
+      header: "Speichern erfolgreich",
+      message: "Die Änderungen wurden erfolgreich übernommen.",
+      buttons: [{text: 'OK',
+                handler: ()=> {
+                  this.router.navigateByUrl('/erziehungsberechtigte-dashboard');
+                },
+              }]
+    });
+    await alert.present();
+
+  }
+
+  abort(){
+    this.router.navigateByUrl('/erziehungsberechtigte-dashboard');
+  }
+
+  async deleteAccount(){
+    const alert = await this.alertController.create({
+      header: "Löschen",
+      message: "Wolle Sie den Account wirklich löschen? Dieser Vorgang kann nicht rückgängig gemacht werden.",
+      buttons: [{text: 'OK',
+      handler: ()=> {
+        this.alertService.presentToast('Der Account wurde gelöscht');
+        this.router.navigateByUrl('/login');
+      }
+    },
+      {text: 'Abbrechen',
+      handler: ()=> {
+        this.router.navigateByUrl('/erziehungsberechtigte-dashboard');
+      }
+    }]
+});
+    await alert.present();
+  }
+
+  async changePassword(){
+    const alert = await this.alertController.create({
+      header: "Passwort ändern erfolgreich",
+      message: "Das Passwort wurde erfolgreich geändert.",
+      buttons: [{text: 'OK',
+      handler: ()=> {
+        this.router.navigateByUrl('/erziehungsberechtigte-dashboard');
+      },
+    }]
+});
+    await alert.present();
   }
 
 }
