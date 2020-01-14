00495766a9bc3bc49685e16d5cbb19faeb72176e
hendrik.garner <garner@posteo.at>
2020-01-14 16:13:04 +0100

    Abfrage vom Backend der Kinder realisiert

---
+++ src/app/models/children.ts
@@ -0,0 +1,14 @@
+class children
+{
+    userType: String;
+    username: String;
+    password: String;
+    email: String;
+    fullname: String;
+    schoolClass: String;
+    phoneNumber: Number;
+    subject: String;
+    iban: String;
+    address: String;
+    schoolCoordinator: false
+}
