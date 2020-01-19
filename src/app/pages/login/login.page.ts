a2a45082a7ab79d32d37be575a06ae6a63babfbb
MaltePetersen <petersen-malte@web.de>
2020-01-19 18:01:12 +0100

    Test funktion, ob die APIs funktionieren

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -3,7 +3,7 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 import { Router } from '@angular/router';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { EnvService } from '../../services/env.service';
-
+let headers = new HttpHeaders();
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -22,6 +22,13 @@ export class LoginPage implements OnInit {
   constructor(private auth: AuthenticationService, private router: Router, public http: HttpClient, private env: EnvService,) { }
 
   ngOnInit() {
+    headers = headers.append("Authorization", "Basic " + 'UGFyZW50X1Rlc3Q6cGFzc3dvcmQ=');
+    headers = headers.append('Content-Type', 'application/json');
+
+    
+    this.http.get('https://development-fjoerde.herokuapp.com/api/parent/childs', {headers}).subscribe((test)=> console.table(test));
+  
+    this.http.get('https://backend.softwareprojekt-kiel.de/api/parent/childs', {headers}).subscribe((test)=> console.table(test));
   }
 
   login() {
