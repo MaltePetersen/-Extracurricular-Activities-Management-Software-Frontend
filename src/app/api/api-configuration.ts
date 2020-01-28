eabcf9eff9ca3dd7cd006d27c00fc93ca238bfe9
DESKTOP-UE8GFTG\Daniel <daniel.last220397@gmail.com>
2020-01-28 15:16:01 +0100

    URL fixed

--- src/app/api/api-configuration.ts
+++ src/app/api/api-configuration.ts
@@ -8,7 +8,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class ApiConfiguration {
-  rootUrl: string = '//backend.softwareprojekt-kiel.de';
+  rootUrl: string = 'https://backend.softwareprojekt-kiel.de';
 }
 
 export interface ApiConfigurationInterface {
